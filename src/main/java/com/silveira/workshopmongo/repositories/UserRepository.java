6a1c438468b9c125c89fe18d4f9612517db80b5e
Andre Silveira <alasandre@gmail.com>
2021-12-07 09:35:14 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/silveira/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.silveira.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.silveira.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	
+	
+}
