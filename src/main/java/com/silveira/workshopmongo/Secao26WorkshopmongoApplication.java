2c9e55a6b1026baf40a08a186a7f8823b4087a51
Andre Silveira <alasandre@gmail.com>
2021-12-07 08:48:06 -0300

    Projeto criado

---
+++ src/main/java/com/silveira/workshopmongo/Secao26WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.silveira.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao26WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao26WorkshopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silveira/workshopmongo/Secao26WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silveira.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao26WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
